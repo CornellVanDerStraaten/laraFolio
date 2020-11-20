caca1e713d78ff02317f7985e3eadf86613a6ee1
Cornell van der Straaten <30183@ma-web.nl>
2020-11-20 11:18:15 +0100

    Pre made all expected routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+/**
+ * DONE Restful Routes
+ */
+
+
+/**
+ * DOING Restful Routes
+ */
+Route::get('/', function () { return view('welcome'); })->name('home');
+
+/**
+ * TODO Restful Routes
+ *
+ * Website
+ * Route::get('/over-mij', 'WebsiteController@showAbout')->name('overMij');
+ *
+ * Admin
+ * Route::get('/admin', 'AdminController@showLogin')->name('admin.showLogin');
+ * Route::post('/admin', 'AdminController@logIn')->name('admin.login');
+ * Route::get('/admin/dashboard', 'AdminController@showDashboard')->name('admin.showDashboard');
+ *
+ * Projecten
+ * Route::get('/projecten', 'ProjectController@index')->name('projecten');
+ * Route::get('/projecten/maken', 'ProjectController@create')->name('projecten.create');
+ * Route::post('/projecten', 'ProjectController@store')->name('projecten.store');
+ * Route::get('/projecten/{slug}', 'ProjectController@show')->name('projecten.show');
+ * Route::get('/projecten/{slug}/aanpassen', 'ProjectController@edit')->name('projecten.edit');
+ * Route::put('/projecten/{slug}', 'ProjectController@update')->name('projecten.update');
+ * Route::delete('/projecten/{slug}', 'ProjectController@destroy')->name('projecten.destroy');
+ *
+ * Artikels
+ * Route::get('/artikels', 'ArtikelController@index')->name('artikels');
+ * Route::get('/artikels/maken', 'ArtikelController@create')->name('artikel.create');
+ * Route::post('/artikels', 'ArtikelController@store')->name('artikel.store');
+ * Route::get('/artikels/{slug}', 'ArtikelController@show')->name('artikel.show');
+ * Route::get('/artikels/{slug}/aanpassen', 'ArtikelController@edit')->name('artikel.edit');
+ * Route::put('/artikels/{slug}', 'ArtikelController@update')->name('artikel.update');
+ * Route::delete('/artikels/{slug}', 'ArtikelController@destroy')->name('artikel.destroy');
+ */
