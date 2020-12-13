48740951ee5c4023b1cc3d665cac9d605c447377
Cornell van der Straaten <30183@ma-web.nl>
2020-12-13 14:30:34 +0100

    Laravel Mix setup & Debug toolbar
    Setup Laravel Mix & Debug Toolbar

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/script.js', 'public/js/mixedJS.js');
 mix.sass('resources/sass/main.scss', 'public/css/style.css');
