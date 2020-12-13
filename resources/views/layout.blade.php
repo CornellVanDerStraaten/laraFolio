dd55c79702fdfc2bb3c455848ec39fe6306fc879
Cornell van der Straaten <30183@ma-web.nl>
2020-12-13 22:27:03 +0100

    Mobile Navbar done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,8 +9,22 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <nav>
-        Dit is de nav in de layout
+    <nav class="nav">
+        <div class="nav__wrapper">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo van Cornell, zijn initialen." class="nav__wrapper-logo">
+            <div class="nav__link-wrapper">
+                <ul class="nav__link-wrapper-ul">
+                    <li class="nav__link-wrapper-link"><a href="#" class="a-tag_component">projecten<span class="slider"></span></a></li>
+                    <li class="nav__link-wrapper-link"><a href="#" class="a-tag_component">artikels</a></li>
+                    <li class="nav__link-wrapper-link"><a href="#" class="a-tag_component nav-button">Contact</a></li>
+                </ul>
+            </div>
+            <div class="nav__burger" id="burgerMenu">
+                <div class="line-top burger-line"></div>
+                <div class="line-middle burger-line"></div>
+                <div class="line-bottom burger-line"></div>
+            </div>
+        </div>
     </nav>
 
     <main>
@@ -22,6 +36,6 @@
     <footer>
         Dit is de footer in de layout
     </footer>
-
+    <script src="{{ asset('/js/mixedJS.js') }}"></script>
 </body>
 </html>
