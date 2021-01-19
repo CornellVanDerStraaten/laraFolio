615565871619a7c3c96c6aacbfd54e33e04286be
Cornell van der Straaten <30183@ma-web.nl>
2021-01-19 21:47:36 +0100

    Header CSS done ( Responsive Included )

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.layout')
 
 @section('content')
+    <header class="home__header">
+        <div class="home__header-positioner">
+            <h1 class="home__header-title">Fullstack Developer.</h1>
+            <span class="home__header-div">
+                <hr class="home__header-hr">
+                <h2 class="home__header-subtitle">Cornell van der Straaten</h2>
+            </span>
+            <a class="home__header-cta a-tag_component">Mijn Werk</a>{{-- TODO Make route --}}
+        </div>
+        <img src="{{ asset('img/header-img.png') }}" class="home__header-img">
+    </header>
+
+
     <h1>Projecten</h1>
     <table>
     @foreach($projects as $project)
@@ -11,4 +24,8 @@
     </tr>
     @endforeach
     </table>
+@endsection
+
+@section('footer-position')
+    footer-relative
 @endsection
