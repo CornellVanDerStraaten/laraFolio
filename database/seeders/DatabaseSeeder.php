b66c3a67491e3af51af2837a8f01334eb7908b1b
Cornell van der Straaten <30183@ma-web.nl>
2021-01-24 12:43:02 +0100

    Made User Seeder For Testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Create user, email: testmail@mail.com Password: testuser
+        $this->call(UserSeeder::class);
+
+        // Generate 5 projects using lorem ipsum and placeholder images
         $this->call(ProjectSeeder::class);
+
+
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Faker;
 
 class UserSeeder extends Seeder
@@ -19,7 +20,12 @@ class UserSeeder extends Seeder
         $faker = Faker\Factory::create('nl_NL');
 
         $user = new User();
-
+        $user->firstname    = $faker->name;
+        $user->email        = 'testmail@mail.com';
+        $user->phone        = $faker->phoneNumber;
+        $user->password     = Hash::make('testuser');
+        $user->role_id      = 1;
+        $user->active       = 1;
         $user->save();
 
     }
