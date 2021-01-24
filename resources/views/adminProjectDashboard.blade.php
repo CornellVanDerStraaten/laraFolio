79ebecd1142961da6a9eda7f1716f3ee3dcf2bde
Cornell van der Straaten <30183@ma-web.nl>
2021-01-24 22:19:36 +0100

    Styled project dashboard

--- resources/views/adminProjectDashboard.blade.php
+++ resources/views/adminProjectDashboard.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.layout')
 
+@section('header-extra')
+    {{-- OwlCarousel   --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/1eb7c10cba.js" crossorigin="anonymous"></script>
+@endsection
+
 @section('content')
     <?php use App\Models\Project; ?>
 <div class="projectDashboard__div">
@@ -10,26 +17,63 @@
         </div>
         <div class="dashboard-buttons">
             <a class="projectDashboard__topLine-button" href="{{ route('projects.create') }}"><p class="nav-button no-margin">Maak Project</p></a>
-            <p class="projectDashboard__topLine-delete">Delete Project</p>
+{{--            <p class="projectDashboard__topLine-delete">Delete Project</p>--}}
         </div>
     </div>
     {{-- Hier komen nog project statistieken, bijvoorbeeld hoeveel bezoekers etc --}}
-    <div class="projectDashboard__allProjects">
+    <div class="projectDashboard__allProjects owl-carousel owl-theme">
 
         @foreach($projects as $project)
-        <div class="projectCard">
-            <div class="projectCard__image-holder">
-                <img src="#" alt="Een foto van het project: {{ $project->title }}">
-
+            <div class="home__project">
+                <div class="home__project-imgContainer">
+                    <img src="{{  str_contains($project->thumbnail_image, 'via.') ? $project->thumbnail_image : asset('storage/' . $project->thumbnail_image) }}" class="home__project-imgContainer-img">
+                </div>
+                <div class="home__project-info">
+                    <h3 class="home__project-info-title" >{{ $project->title }}</h3>
+                    <p class="home__project-info-keywords">{{ $project->keywords }}</p>
+                    <div class="home__project-info-button-positioner">
+                        <a class="home__project-info-button nav-button" href="{{ route('projecten.edit', ['slug' => $project->slug])  }}">Edit Project</a>
+                    </div>
+                </div>
+                <div class="delete_icon_div" onclick="document.getElementById('form{{$project->id}}').submit()">
+                        <i class="fas fa-trash-alt delete_icon"></i>
+                    <form id="form{{$project->id}}" action="{{ route('projecten.destroy', ['slug' => $project->slug]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </div>
             </div>
-            <div class="projectCard_card-info">
-
-            </div>
-
-        </div>
         @endforeach
     </div>
 </div>
 
 
 @endsection
+@section('script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous"></script>
+    {{--  Init Carousel  --}}
+    <script>
+        const carouselArrow = `<img src="{{ asset('img/carousel-arrow.svg') }}" class="carousel-arrow-prev">`;
+        const carouselArrowRight = `<img src="{{ asset('img/carousel-arrow.svg') }}" class="carousel-arrow-next">`;
+
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                center: true,
+                items:1,
+                loop:true,
+                nav:true,
+                navText:[carouselArrow, carouselArrowRight],
+                lazyload:true,
+                responsive:{
+                    900:{
+                        items:3
+                    }
+                }
+            });
+        });
+    </script>
+
+
+@endsection
+
--- resources/views/viewProject.blade.php
+++ resources/views/viewProject.blade.php
@@ -1,14 +1,53 @@
 @extends('layouts.layout')
 
 @section('content')
-<div>
-    <table class="table">
-        <tr class="row"><td><h1>{{ $project->title }}</h1></td></tr>
-        <tr><td>{{ $project->keywords }}</td></tr>
-        <tr><td>{{ $project->content }}</td></tr>
-        <tr><td>{{ $project->published_date }}</td></tr>
-        <tr><td><img src="{{ asset('storage/' . $project->thumbnail_image ) }}" alt="Foto"></td></tr>
+    <div class="createProject__container">
+        <div class="project">
+            <div class="project__top">
+                <h1 class="project__top-h1">{{ $project->title }}</h1>
+                <h2 class="project__top-h2">{{ $project->keywords }}</h2>
+                <div class="project__top-links">
+                    {{-- If no github link, dont show button --}}
+                    @if( !$project->github_link == null)
+                        <a class="project__top-link" href="{{ $project->github_link }}" target="_blank"><button>Github Link</button></a>
+                    @endif
+                    {{-- If no live link, dont show button --}}
+                    @if( !$project->live_link == null)
+                        <a class="project__top-link" href="{{ $project->live_link }}" target="_blank"><button>Live Link</button></a>
+                    @endif
+                </div>
+            </div>
 
-    </table>
-</div>
+            <div class="cp__thumb-holder project__thumbnail" style="background-image: url('{{  str_contains($project->thumbnail_image, 'via.') ? $project->thumbnail_image : asset('storage/' . $project->thumbnail_image) }}');"></div>
+            <div class="cp__extra-info project__extra-info">
+                <span class="project__extra-info-item">
+                    <p class="project__extra-item-title">Developers</p>
+                    <p class="project__extra-developers project__extra-item">{{ $project->developers }}</p>
+                </span>
+                <span class="project__extra-info-item">
+                    <p class="project__extra-item-title">Vormgevers</p>
+                    <p class="project__extra-developers project__extra-item">{{ $project->vormgevers }}</p>
+                </span>
+                <span class="project__extra-info-item">
+                    <p class="project__extra-item-title">Taal</p>
+                    <p class="project__extra-developers project__extra-item">{{ $project->taal }}</p>
+                </span>
+            </div>
+
+            <div class="project__contentarea">
+                {!! $project->content !!}
+            </div>
+
+            <div class="cp__extra-images">
+                <div class="cp__extra-image" style="background-image:  url('{{  str_contains($project->thumbnail_image, 'via.') ? $project->thumbnail_image : asset('storage/' . $project->images[0]->link) }}');" id="extra_project_image_left"></div>
+                <div class="cp__extra-image" style="background-image:  url('{{  str_contains($project->thumbnail_image, 'via.') ? $project->thumbnail_image : asset('storage/' . $project->images[1]->link) }}');" id="extra_project_image_left"></div>
+                <div class="cp__extra-image" style="background-image:  url('{{  str_contains($project->thumbnail_image, 'via.') ? $project->thumbnail_image : asset('storage/' . $project->images[2]->link) }}');" id="extra_project_image_left"></div>
+            </div>
+        </div>
+    </div>
 @endsection
+
+@section('footer-position')
+    footer-relative
+@endsection
+
